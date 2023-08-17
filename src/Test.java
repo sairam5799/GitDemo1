550cf169b843c643a8542efc85f87ea7afae10e1
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-08-17 10:13:18 +0530

    commit

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		String hungry = "Yes";
+			 if (hungry == "Yes")
+				 System.out.println("Eat Something....");
+			 else
+			 {
+				 System.out.println("Do Something....");
+			 }
+
+			}
+
+
+
+	}
+
